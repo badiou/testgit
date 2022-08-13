9602523174cc52882a62e40945429627edb7328f
ourobadiou@gmail.com <ourobadiou@gmail.com>
2022-08-13 17:25:30 +0000

    test session 4 test 1

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ def home():
         return render_template('index.html')
     return render_template('index.html')
 
+
+
+
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run(port=5000,debug=True)
